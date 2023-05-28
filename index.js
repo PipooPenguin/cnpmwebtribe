6043cf15cbf0ae686e9376d7e5ff73b3cf6a52fd
UIT-21522599 <21522599@gm.uit.edu.vn>
2023-05-28 14:50:19 +0700

    cart đó

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/", (req, res) => res.redirect("localhost:3000/menu"));
+app.get("/", (req, res) => res.render('home.html'));
 app.use("/menu", menu);
 app.use("/category", category);
 app.use("/user", user);
