6a97ba7efab88dd043b47d12b77fb86764f96cc8
UIT-21522599 <21522599@gm.uit.edu.vn>
2023-05-29 19:28:51 +0700

    category controller coding

--- modules/category/category.controller.js
+++ modules/category/category.controller.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const Dish = require('../menu/menu.model.js')
 
-
-router.get("/category", async (req, res) => {
+router.get("/", async (req, res) => {
+  console.log("cataloger.controller GET/categoy:", req.query);
   const { q } = req.query;
   const dish = await Dish.find({ type: q });
   res.render("category.html", { dish });
