a87a69c75e615bd5193120c7139e1b8f321a1cdf
UIT-21522599 <21522599@gm.uit.edu.vn>
2023-06-02 15:19:19 +0700

    setup admin environment

---
+++ modules/admin/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejs = require("ejs");
+const mongodb = require("../mongodb/mongodb.service");
+const dashboard = require("./modules/dashboard/dashboard.controller.js");
+
+const { engine } = require("express-handlebars");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+
+mongodb.connect();
+
+app.engine("html", require("ejs").renderFile);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", dashboard);
+// app.use('/product',product);
+// app.use('/order')
+
+app.listen(80, (req, res) => {
+  console.log("App is listening on port 80");
+});
